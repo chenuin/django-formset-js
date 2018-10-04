46c001e3dfdf238ebe9a3d3e44eec9d6f8f9aed5
yuyin <yuyin0036@gmail.com>
2018-10-04 15:38:20 +0000

    improve formset layout

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
